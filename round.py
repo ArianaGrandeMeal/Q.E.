f9187318c720eb7551763c03fca6ba40201d3d66
ArianaGrandeMeal <trippsellers22@gmail.com>
2024-01-29 19:00:54 -0500

    Changes made to round.py: - fixed typo in _reveal__new_company_tile - added valid bid verification to opening bid and private bid methods - removed check_for_tied_bids method and incorporated tied bid evaluation process into check_for_max_bid

--- round.py
+++ round.py
@@ -8,6 +8,7 @@ class Round:
         self._current_tile = round_tile
         self._opening_bid = None
         self._bids = dict()
+        self._tied_bids = 0
 
         self._reveal_new_company_tile()
         self._get_open_bid()
@@ -18,69 +19,67 @@ class Round:
         print(f"Round {self._round_number}.  The company tile up for auction is:\n")
         print(f"Country: {self._current_tile[0]}") 
         print(f"Industry: {self._current_tile[1]}")
-        print(f"Worth {self._current_tile[3]} victory points.\n")
+        print(f"Worth {self._current_tile[2]} victory points.\n")
         
     def _get_open_bid(self):
         print(f"{self._auctioneer}, you are the autioneer for this round.\n")
         self._opening_bid = int(input("Please enter your opening bid of at least $1 for the current tile."))
-        while self._opening_bid < 1:
+        # verify valid bid amount
+        while self._opening_bid < 1 and isinstance(self._opening_bid, int):
             self._opening_bid = int(input("Invalid input.  Please enter your opening bid of at least $1: "))
         self._bids[self._auctioneer] = self._opening_bid
         
     def _get_private_bid(self, active_player):
             print(f"{active_player}, please enter your bid.  ")
             if active_player in self._zero_bids:
-                bid = int(input("You may not bid zero this round. "))
-                while bid < 1:
-                    bid = int(input("Invalid bid. Please enter a bid greater than zero. "))
+                bid = int(input("You may not bid zero this round. Please bid a whole number greater than zero"))
+                while bid < 1 and isinstance(bid, int):
+                    bid = int(input("Invalid bid. Please bid a whole number greater than zero. "))
             else:
-                bid = int(input("You may place a bid of zero this round. "))
-                while bid < 1:
-                    bid = int(input("Invalid bid. Please enter a bid greater than or equal to zero. "))
+                bid = int(input("You may place a bid of zero this round. Otherwise, your bid must be a whole number."))
+                while bid < 0:
+                    bid = int(input("Invalid bid. Please bid a whole number greater than or equal to zero. "))
             self._bids[active_player] = bid
     
-    def _check_for_tied_bids(self):
-        found = 0
-        tied_bids = []
-        max_bid = max(self._bids.values())
-        for bid in self._bids.values():
-            if bid == max_bid:
-                found += 1
-                tied_bids.append([max_bid])
-        if found < 1:
-
-
-    
+    # finds max bid, evalutes for tied bids.  
+    # If tied, calls _get_private_bid on tied players, then calls self to re-evaluate bids.
+    # If three tied bids occur in a row, tied bids are eliminated and next highest bid wins.
     def _determine_winning_bid(self):
-        
+        # Determine highest bid, regardless of tie.
         max_bid = max(self._bids.values())
-        bidder = {i for i in self._bids if self._bids[i] == max_bid}
-        winning_bid = [max_bid, bidder]
         
-        # check for tied max bids
+        # Check for tied max bids
         found = 0
         tied_bidders = []
-        for bid in self._bids.values():
+        for bidder, bid in self._bids:
             if bid == max_bid:
                 found += 1
-                tied_bidders.append(winning_bid)
-        if found < 1:
-            
+                winning_bid = [bidder, bid]
+                tied_bidders.append(bidder)
+        # If tied, increment tied bid counter.
+        if found > 1:
+            self._tied_bids += 1
         
-
+        # Check for 3 tied bids in a row.
+        # If true, eliminate tied bids and determine highest remaining bid. 
+            if self._tied_bids == 3:
+                print("There are three tied bids in a row.")
+                print("The tied bids are eliminated and the highest remaining bid wins.")
+                for bidder in tied_bidders:
+                    del self._bids[bidder]
+                self._determine_winning_bid()
         
+        # Otherwise, announce tied bid,vremove tied bids from bid dictionary, get new bids and evaluate.
+            print("The following players tied for the max bid and must bid again.")
+            print("Your bid may be more than, less than, or equal to your previous bet.")
+            print("If there are three tied bids in a row, the highest non-tied bid wins the auction.")
+            for bidder in tied_bidders:
+                del self._bids[bidder]
+                self._get_private_bid(bidder)
+                self._determine_winning_bid()
+        
+        # If no tied bids, return winning bid
         return winning_bid
-
         
-        
-
-
-            
-                
-                    
-                
-                
-                
-
-
+       
         
